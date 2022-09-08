bfeeaf6ac86007b75dd00db93a56fc8bc8ae024e
wissem007 <alouiwiss@gmail.com>
2022-09-08 15:03:06 +0100

    first commit

--- app-service/main.tf
+++ app-service/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group" "example" {
   location = "West Europe"
 }
 
-resource "azurerm_app_service_plan" "example" {
+resource "azurerm_service_plan" "example" {
   name                = "example-appserviceplan"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name

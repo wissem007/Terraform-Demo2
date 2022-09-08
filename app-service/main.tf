e99c96ca4268077efdb2b99b3381c3c0ec284639
wissem007 <alouiwiss@gmail.com>
2022-09-08 15:13:11 +0100

    first commit

--- app-service/main.tf
+++ app-service/main.tf
@@ -4,34 +4,18 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_service_plan" "example" {
-  name                = "example-appserviceplan"
-  location            = azurerm_resource_group.example.location
+  name                = "example"
   resource_group_name = azurerm_resource_group.example.name
-
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
+  location            = azurerm_resource_group.example.location
+  os_type             = "Linux"
+  sku_name            = "P1v2"
 }
 
-resource "azurerm_app_service" "example" {
-  name                = "example-app-service"
-  location            = azurerm_resource_group.example.location
+resource "azurerm_linux_web_app" "example" {
+  name                = "example"
   resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_app_service_plan.example.id
+  location            = azurerm_service_plan.example.location
+  service_plan_id     = azurerm_service_plan.example.id
 
-  site_config {
-    dotnet_framework_version = "v4.0"
-    scm_type                 = "LocalGit"
-  }
-
-  app_settings = {
-    "SOME_KEY" = "some-value"
-  }
-
-  connection_string {
-    name  = "Database"
-    type  = "SQLServer"
-    value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
-  }
+  site_config {}
 }
